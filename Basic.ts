63041a03fe1aa3c55288dc9917b1f21aa7d5f229
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-09-27 11:22:39 +0700

    .

---
+++ Basic.ts
@@ -0,0 +1,45 @@
+/*
+* Static Type-Checking *
+Verify Types Of Variables & Functions Before Running The Code
+Catch Potential Errors Before Runtime <> JavaScript - Check Types At Runtime
+
+* Non-Exception Failures *
+JavaScript - Not Throw Errors Immediately Even With Logical Issues
+TypeScript - Catch Issues: Undefined Properties | Non-Callable Values
+
+* Types For Tooling *
+Provide Code Completion + Error Highlighting + "Quick Fixes" In Editors
+Help Prevent Bugs By Suggesting & Detecting Errors
+
+* The TypeScript Compiler *
+Convert TypeScript (.ts) To JavaScript (.js)
+Enforce Static Type-Checking & Ensure Compatibility With JavaScript
+
+* Emitting With Errors *
+Highlight Errors <> Not Stop Code Execution By Default
+--noEmitOnError - Stop Emitting JavaScript Files If Errors Are Present
+
+* Explicit Types *
+Type Annotations To Specify Expected Types For Variables/Functions
+Improve Code Clarity & Enable Better Type-Checking By TypeScript
+
+* Erased Types *
+Type Annotations Exist Only At Compile-Time
+Strip Annotations In The Output JavaScript
+
+* Downleveling *
+Compile Modern ECMAScript Features (ES6/ES2015) To Older Versions (ES5)
+Ensure Compatibility With Older Browsers While Supporting Newer Language Features
+
+* Strictness *
+Strict Type-Checking Flags For More Rigorous Error-Checking
+Activate Stricter - Check For Null/Undefined & Type Inference
+
+* noImplicitAny *
+Throw Error If A Variable's Type Is Inferred As `Any`
+Encourage Explicit Type Annotations & Prevent Type Safety Loss
+
+* strictNullChecks *
+Treat `Null` & `Undefined` As Separate Types
+Require Explicit Handling Of `Null` & `Undefined` To Avoid Bugs
+*/
